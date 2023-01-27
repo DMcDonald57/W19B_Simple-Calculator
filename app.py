a290a7163a802a22481cf8bdecef8ed9e5a08d6d
DMcDonald57 <d.mcdonald579@gmail.com>
2023-01-26 20:45:05 -0700

    Opening message created to make selection

---
+++ app.py
@@ -0,0 +1,11 @@
+import addition
+import division
+import multiplication
+import subtraction
+
+print("Welcome to the simple calculator.")
+print("Please select one of the following: ")
+print("1: Addition")
+print("2: Subtraction")
+print("3: Multiplication")
+print("4: Subtraction")
