0f5da1aa32528bbfb644c66610db712d44fd43ba
DMcDonald57 <d.mcdonald579@gmail.com>
2023-01-26 21:02:59 -0700

    working the connections between modules

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import addition
-import division
-import multiplication
-import subtraction
+from addition import add
+from subtraction import minus
+from multiplication import multiply
+from division import divide
 
 print("Welcome to the simple calculator.")
 print("Please select one of the following: ")
@@ -11,7 +11,6 @@ print("3: Multiplication")
 print("4: Subtraction")
 
 selection = input("Please enter your calculation choice:\n")
-
 first_num = input("Please enter your first number:\n")
-
 second_num = input("Please enter your second number:\n")
+print("Your Result:\n"+result)
